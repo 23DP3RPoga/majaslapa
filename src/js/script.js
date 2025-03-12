4117c33b76cb01fefbc3389588e4ec97f34cf0d4
23DP3RPoga <A231753RP@RVT.LV>
2025-03-12 17:08:23 +0200

    Add initial HTML structure, CSS styles, and JavaScript for fetching cat facts

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("demo").innerHTML = "";
+    fetch("https://catfact.ninja/fact")
+        .then(res => res.json())
+        .then(data => {
+            let fact = data.fact;
+            console.log(fact);
+            document.getElementById("demo").innerHTML = `<div class="fact-container">${fact}<img src="src/images/cat-space_12435967.png" class="cat"/></div>`;
+        });
+    
+    function catfact() {
+        document.getElementById("demo").innerHTML = "";
+        fetch("https://catfact.ninja/fact")
+            .then(res => res.json())
+            .then(data => {
+                let fact = data.fact;
+                console.log(fact);
+                document.getElementById("demo").innerHTML = `<div class="fact-container">${fact}<img src="src/images/cat-space_12435967.png" class="cat"/></div>`;
+            });
+    }
+
+    document.getElementById("anotherone").addEventListener("click", catfact);
+    document.getElementById("copyright").innerHTML = `&copy; ${new Date().getFullYear()} MoonStudio`;
+
+
+});
+
+
+
+
